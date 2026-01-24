022b3dc06886152207b12c4afc6d057012d5f414
CahyoDev <121766510+CahyoDev@users.noreply.github.com>
2026-01-25 05:42:50 +0700

    Update Spotify playlist URL in data.js

--- src/config/data.js
+++ src/config/data.js
@@ -102,7 +102,7 @@ module.exports = {
   spotify: {
     enabled: true,
     title: "My Playlist",
-    playlistUrl: "https://open.spotify.com/embed/playlist/3eydlGlNdYVNFIIfLmublg?utm_source=generator&theme=0"
+    playlistUrl: "https://open.spotify.com/embed/playlist/2gRz8DBnVSIKa6pCah73ZR?si=aogo0CPYTqyaNGHqMdPQXw&pi=cGpBM1loRe-9S"
   },
 
   footer: {
